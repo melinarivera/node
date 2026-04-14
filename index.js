9f87e443456ae6de369a37caba8ee35f6805bea9
Melina <melina.rivera@gmail.com>
2026-04-14 17:02:12 +0200

    mongo  conectado

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Node funcionandoooooooo");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB conectado");
+  } catch (error) {
+    console.error("Error:", error.message);
+    process.exit(1);
+  }
+}
+
+connectDB();
